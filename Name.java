e4473ce3930c3099af53bd946f4b5dbf523f9db1
Abash Ansari <Ansariabash2004@gmail.com>
2025-01-24 07:22:53 -0800

    Day 1 code

---
+++ Name.java
@@ -0,0 +1,10 @@
+//Q.write a program to print your name,userinput
+
+import java.util.*;
+public class main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your name :");
+        String name = sc.nextLine();
+        System.out.println("Your name is :"+name);          }
+       }
