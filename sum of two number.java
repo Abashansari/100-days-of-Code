16beae3dfab1e3846ac25d4185dafef96c922474
Abash Ansari <Ansariabash2004@gmail.com>
2025-01-25 04:32:33 -0800

    Day 2 code

---
+++ sum of two number.java
@@ -0,0 +1,17 @@
+//Q. Write a program to sum two number,from user import.
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       // creating a Scanner object.
+      Scanner sc = new Scanner(System.in);
+      
+   System.out.println("Enter the first number:");
+           int a = sc.nextInt();
+           
+System.out.println("Enter the second number:");         int b = sc.nextInt();
+      
+        int sum = a+b;
+  System.out.println("The sum of two number is :"+sum);
+         }
+    }
