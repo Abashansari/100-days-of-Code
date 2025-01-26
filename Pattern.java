b49a8e90538c845d54221280dab945159c72d210
Abash Ansari <Ansariabash2004@gmail.com>
2025-01-26 07:58:53 -0800

    Code's day 3

---
+++ Pattern.java
@@ -0,0 +1,11 @@
+//Q. print the pattern  of "*"
+
+public class Main {
+	public static void main(String[] args) {
+		//Draw a pattern
+		System.out.println("*");
+		System.out.print("**\n");
+		System.out.println("***");
+		System.out.print("****");
+	}
+}
