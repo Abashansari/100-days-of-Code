f37eb7059e4636566d1f1b513afc46c56ad2cffd
Abash Ansari <Ansariabash2004@gmail.com>
2025-02-03 05:07:14 -0800

    Day 7 code

---
+++ calculate equation.java
@@ -0,0 +1,12 @@
+//Q.calculate axb/a-b ,where a = 10 and b = 5.
+
+public class main{
+      public static void main(String[]args){
+int a = 10;
+int b = 5 ;
+int cal = (a*b)/(a-b); // BODMAS not applied in java.
+System.out.println(cal);
+      }
+}
+
+                     
