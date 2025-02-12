547ae09ed57dd2730604473327831071e8a8a5a3
Abash Ansari <Ansariabash2004@gmail.com>
2025-02-12 04:20:38 -0800

    Day 11 code

---
+++ Solid Rectangle.java
@@ -0,0 +1,14 @@
+//Q. print the pattern of Solid Rectangle.
+// 4 rows and 5 coloum
+
+public class Main {
+	public static void main(String[] args) {
+	System.out.println("pattern of  Solid Rectangle.");
+	for( int i = 1; i <= 4 ; i++){
+	    	for( int j = 1; j <= 4 ;j++){
+	    	    System.out.print("*");
+	    	}
+	    System.out.println("*");
+   	}
+	}
+}
