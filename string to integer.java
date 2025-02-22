d3b8bf434e107bb45493bb4007388150b11789c3
Abash Ansari <Ansariabash2004@gmail.com>
2025-02-21 22:54:57 -0800

    Day 17 code

---
+++ string to integer.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		// initializng string value.
+		String a = "99";
+		String b = "1";
+		//converting into integer
+		int num1 = Integer.parseInt(a);
+		int num2 = Integer.parseInt(b);
+		// calculating sum
+		int sum =	num1 + num2;
+
+		System.out.println(sum);
+	}
+}
