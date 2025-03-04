ba09347057b53ae968f99c0101de31b3d734782e
Abash Ansari <Ansariabash2004@gmail.com>
2025-03-03 22:55:48 -0800

    Day 22 code

---
+++ print 1 to 10 using do-while loop.java
@@ -0,0 +1,11 @@
+//Q.print 0 to 10 number using do-while loop.
+
+public class main{
+    public static void main(String[]args){
+         int i = 0;
+         do{
+             System.out.println(i);
+             i++;
+         }while(i<=10);
+    }
+}
