9fa1103619f7f6429b0b3e006b971fc67fbf2d2c
Abash Ansari <Ansariabash2004@gmail.com>
2025-04-09 23:09:39 +0530

    Day 52 code

---
+++ user-defined function.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void myFunction() {
+		System.out.println("This is a user-defined function..");
+	}
+	public static void main(String[] args) {
+		myFunction(); //calling function
+	}
+}
