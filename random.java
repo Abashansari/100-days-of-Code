10d296f6b319aaed92a7394981e88973c104ca08
Abash Ansari <Ansariabash2004@gmail.com>
2025-04-25 06:11:39 -0700

    Day 68 code

---
+++ random.java
@@ -0,0 +1,5 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Print a random number greater than or equal to 0.0 and less than 1.0 :" + Math.random());
+	}
+}
