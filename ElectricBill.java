c520dc2fa7ad9c8fe58448b10b9bd59179bdb5b0
Abash Ansari <Ansariabash2004@gmail.com>
2025-05-01 20:16:12 +0530

    Day 74 code

---
+++ ElectricBill.java
@@ -0,0 +1,23 @@
+package practise;
+
+public class ElectricBill {
+    public static void main(String[]args){
+        int unit =123;
+        int bill = 0;
+        if(unit >= 100){
+            if(unit >= 200){
+                if(unit >= 300){
+                    bill = unit * 8;
+                }
+                else
+                    bill = unit * 6;
+            }
+            else
+                bill = unit * 5;
+        }
+        System.out.println("CHENNAI ELECTRICITY LTD,CHENNAI");
+        System.out.println("UNIT Consumed :"+unit);
+        System.out.println("Total Bill :"+bill);
+    }
+}
+
