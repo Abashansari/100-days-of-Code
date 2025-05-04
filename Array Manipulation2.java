f1fafcc47e87b17820fddbfc685d42c559c78a23
Abash Ansari <Ansariabash2004@gmail.com>
2025-05-04 20:03:12 +0530

    Day 77 code

---
+++ Array Manipulation2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class Main {
+	public static void main(String[] args) {
+		int[] array1 = {1, 2, 3};
+		int[] array2 = {1, 2, 3};
+		boolean isEqual = Arrays.equals(array1, array2);
+		System.out.print(isEqual);
+	}
+}
