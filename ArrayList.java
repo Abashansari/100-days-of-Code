5d4d228326996236bc8bbe355213f91631266421
Abash Ansari <Ansariabash2004@gmail.com>
2025-05-13 22:41:30 +0530

    Day 86 code

---
+++ ArrayList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("Abash", "Bikash", "Ajay"));
+		arrayList.add("Rejoel");
+		System.out.println(arrayList);
+	}
+}
