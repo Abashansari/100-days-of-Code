4b9aeaf1412430dcf7fdf4c6191fc3eea5d480cf
Abash Ansari <Ansariabash2004@gmail.com>
2025-05-19 12:18:00 -0400

    Day 92 code

---
+++ operator manipulation 5.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 7;
+		int result = a | b;
+		System.out.println(result);
+	}
+}
+/* Logic behind this
+a = 5 = 0101 (In Binary)
+b = 7 = 0111 (In Binary)
+
+Bitwise OR Operation of 5 and 7
+  0101
+| 0111
+ ________
+  0111  = 7 (In decimal)
+  */
