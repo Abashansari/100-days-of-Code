56a4c8f8c5cde4a9800e35e681bab4aff106086e
Abash Ansari <Ansariabash2004@gmail.com>
2025-05-21 21:38:51 +0530

    Day 94 code

---
+++ Basic Boolean Operations.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean result1 = (5 > 2);
+		boolean result2 = (10 == 15);
+		boolean result3 = (true && false);
+		boolean result4 = (true || false);
+
+		System.out.println("Is 5 greater than 2? " + result1);
+		System.out.println("Is 10 equal to 15? " + result2);
+		System.out.println("true AND false: " + result3);
+		System.out.println("true OR false: " + result4);
+	}
+}
