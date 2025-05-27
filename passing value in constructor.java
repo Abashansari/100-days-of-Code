1adee2aa040c98ccaf6d91953e3bcc51153a96cc
Abash Ansari <Ansariabash2004@gmail.com>
2025-05-27 12:02:53 -0400

    Day 99 code

---
+++ passing value in constructor.java
@@ -0,0 +1,14 @@
+public class Dog {
+	String name;
+
+	// Constructor
+	public Dog(String name) {
+		this.name = name;
+	}
+
+	public static void main(String[] args) {
+		Dog myDog = new Dog("Buddy"); '
+		System.out.println("My dog'
+						   s name is: " + myDog.name);
+	}
+}
